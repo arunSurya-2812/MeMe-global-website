bc8bbb30286bab6ffd3233d975f4f516d5a1a703
divyamohanraju07 <dvmhn07@gmail.com>
2021-07-19 11:47:20 +0530

    dependency changes

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
  
