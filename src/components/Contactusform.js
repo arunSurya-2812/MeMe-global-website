0872f5260a69d07f29ff59d0c9efc9b1469a9385
divyamohanraju07 <dvmhn07@gmail.com>
2021-07-29 16:56:50 +0530

    fiber and contact form change

--- src/components/Contactusform.js
+++ src/components/Contactusform.js
@@ -12,6 +12,7 @@
  **/
 import React, { Component } from "react";
 import Contactusgroup from "./img/Contactusgroup.png";
+import { Card, Row, Col } from "react-bootstrap";
 
 export default class Leathers extends Component {
   constructor(props) {
@@ -25,45 +26,53 @@ export default class Leathers extends Component {
 
   render() {
     return (
-      <div className="Contactusformoutline">
-        <h1 className="Contactus">Contact Us</h1>
-        <div>
-          <img
-            src={Contactusgroup}
-            alt="contactUs"
-            className="Contactusformimg"
-          />
-        </div>
-        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)}>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={this.onNameChange.bind(this)}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="email"
-              placeholder="Contact"
-              className="form-control"
-              aria-describedby="emailHelp"
-              value={this.state.email}
-              onChange={this.onEmailChange.bind(this)}
-            />
-          </div>
-          <div className="form-group">
-            <textarea
-              className="form-control"
-              placeholder="Message"
-              value={this.state.message}
-              onChange={this.onMessageChange.bind(this)}
-            />
-          </div>
-          <button className="Contactusbtn">Submit</button>
-        </form>
+      <div>
+        <Card className="contactCard">
+          <Card.Title className="contactTitle">Contact Us</Card.Title>
+          <Card.Body>
+            <Row>
+              <Col md="6" lg="6">
+                <img
+                  src={Contactusgroup}
+                  alt="contactUs"
+                  className="contactImg"
+                />
+              </Col>
+              <Col md="6" lg="6">
+                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)}>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Name"
+                      value={this.state.name}
+                      onChange={this.onNameChange.bind(this)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      placeholder="Contact mailId"
+                      className="form-control"
+                      // aria-describedby="emailHelp"
+                      value={this.state.email}
+                      onChange={this.onEmailChange.bind(this)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      className="form-control"
+                      placeholder="Message"
+                      value={this.state.message}
+                      onChange={this.onMessageChange.bind(this)}
+                    />
+                  </div>
+                  <button className="Contactusbtn">Submit</button>
+                </form>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
--- src/components/Fiber.js
+++ src/components/Fiber.js
@@ -14,86 +14,96 @@
 import "./Components.css";
 import Contactusform from "./Contactusform";
 import { FaDownload } from "react-icons/fa";
-import React, { Component } from "react";
+import React from "react";
 import yarn from "./img/yarn.png";
-import { Image } from "react-bootstrap";
+import { Image, Row, Col, Card, Button } from "react-bootstrap";
 import Linen from "./img/Linen.jpg";
 import cotton1 from "./img/cotton1.jpg";
 import bananafibre from "./img/bananafibre.jpg";
 import fabproduct4 from "./img/fabproduct4.png";
 import fabproduct1 from "./img/fabproduct1.png";
-import Footer from './Footer'
-export default class Fiber extends Component {
-  render() {
-    return (
-      // <div className="container-fluid">
-      <div>
-        <img src={yarn} className="fabimage" alt="Fiber-main" />
-          <div className="facbricdiscription">
-            <h1
-              style={{
-                fontFamily: "Poppins,Bold",
-                fontSize: "26px",
-                textAlign: "center",
-                color: "black",
-              }}
-            >
-              ABOUT US
-            </h1>
-            <p
-              style={{
-                fontFamily: "Poppins",
-                fontSize: "20px",
-                textAlign: "center",
-                color: "black",
-              }}
-            >
-              We are India’s leading manufacturers of leather goods expertise in
-              export of high end quality of finished Leather Bags, footwear and
-              various leather accessories To gain our Customer’s trust,
-              expectations and requirements and build a long term relationship
-              with them is our main Objective We are located in CHENNAI RANIPET
-              INDIA MEME INDIA We offer leather goods that are both fashionable
-              and sustainable. We as leather and leather goods exporter in
-              India, ship to overseas destinations like Canada, Germany, France,
-              Australia, Russia, USA and other European Countries. We deal in
-              Leather bags, Belts ,wallets and all kinds of small leather goods
-              for both Men & Women segment. MEME Leather has a dedicated leather
-              tannery in the southern Indian city of Chennai, which can process
-              15,000 skins each day. This backward integration enables MEME to
-              create new finishes and fashions in leather, which has been a key
-              element in growing our business. This facility has multiplied its
-              capacity to serve as our hub for leather sourcing and finishing.
-            </p>
-          </div>
-          <button className="Fiberpdfbtn">
-            <FaDownload className="pdficon" />
-            Download pdf
-          </button>
-        <div>
-          <h1 className="Fiberproducts1">Our Products</h1>
-          <Image src={cotton1} className="Fiberproductcol2" />
-          <h1 className="Fiberourproduct">Cotton</h1>
-          <Image src={Linen} className="Fiberproductcol1" />
-          <h1 className="Fiberourproduct2"> Linen</h1>
-          <Image src={bananafibre} className="Fiberproductcol3" />
-          <h1 className="Fiberourproduct1"> Fibre</h1>
-          <Image src={fabproduct4} className="Fiberproductcol4" />
-          <h1 className="Fiberourproduct3"> Silk</h1>
-          <Image src={fabproduct1} className="Fiberproductcol5" />
-          <h1 className="Fiberproduct4"> Lotus</h1>
-        </div>
-        <button className="Fiberwebsitebtn">View Website</button>
-
-        <div >
-        <Contactusform/>
-        </div>   
-        
-        <div>
-          <Footer/>
-        </div> 
-      
-      </div>
-    );
-  }
+import Footer from "./Footer";
+export default function Fiber() {
+  return (
+    <>
+      <Row>
+        <Col md="12" lg="12">
+          <Card className="imgCard">
+            <Card.Img src={yarn} alt="Fiber-main" className="bgImg"></Card.Img>
+          </Card>
+          <Card className="contentCard">
+            <Card.Body>
+              <h1 className="cardTitle">ABOUT US</h1>
+              <Card.Text className="cardContent">
+                <p>
+                  We are India’s leading manufacturers of leather goods
+                  expertise in export of high end quality of finished Leather
+                  Bags, footwear and various leather accessories. To gain our
+                  Customer’s trust, expectations and requirements and build a
+                  long term relationship with them is our main Objective. We are
+                  located in CHENNAI RANIPET INDIA MEME INDIA. We offer leather
+                  goods that are both fashionable and sustainable. We as leather
+                  and leather goods exporter in India, ship to overseas
+                  destinations like Canada, Germany, France, Australia, Russia,
+                  USA and other European Countries. We deal in Leather bags,
+                  Belts ,wallets and all kinds of small leather goods for both
+                  Men & Women segment. <br /> MEME Leather has a dedicated
+                  leather tannery in the southern Indian city of Chennai, which
+                  can process 15,000 skins each day. This backward integration
+                  enables MEME to create new finishes and fashions in leather,
+                  which has been a key element in growing our business. This
+                  facility has multiplied its capacity to serve as our hub for
+                  leather sourcing and finishing.
+                </p>
+              </Card.Text>
+              <Button className="cardButton">
+                <FaDownload className="pdfIcon" />
+                Download PDF
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <br />
+      <Row style={{ textAlign: "center" }}>
+        <h1>Our Products</h1>
+        <Col md="4" lg="4">
+          <Image src={cotton1} className="Fiberproductcol1" roundedCircle />
+          <h1>Cotton</h1>
+        </Col>
+        <Col md="4" lg="4">
+          <Image src={Linen} className="Fiberproductcol2" roundedCircle />
+          <h1> Linen</h1>
+        </Col>
+        <Col md="4" lg="4">
+          <Image src={bananafibre} className="Fiberproductcol3" roundedCircle />
+          <h1> Fibre</h1>
+        </Col>
+      </Row>
+      <Row style={{ textAlign: "center" }}>
+        <Col md="6" lg="6">
+          <Image src={fabproduct4} className="Fiberproductcol4" roundedCircle />
+          <h1> Silk</h1>
+        </Col>
+        <Col md="6" lg="6">
+          <Image src={fabproduct1} className="Fiberproductcol5" roundedCircle />
+          <h1> Lotus</h1>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col md="12" lg="12">
+          <Button className="websiteButton">View Website</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col md="12" lg="12">
+          <Contactusform />
+        </Col>
+      </Row>
+      {/* <div>
+          <Footer />
+        </div> */}
+    </>
+  );
 }
