06133c8574aaa9706fad06c3d26a5f095b650544
arunSurya-2812 <81164749+arunSurya-2812@users.noreply.github.com>
2021-08-09 15:45:31 +0530

    Footer set

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import logo from "../img/logo.png";
-import Ashoka from "../img/Ashoka.jpg";
-import { FaInstagram } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
+import GoogleStore from '../img/GoogleStore.png'
+import AppleStore from '../img/AppleStore.png' 
+import Twitter from '../img/Twitter.png' 
+import Original from '../img/Original.png'
+import Days from '../img/Days.png'
+import Youtube from '../img/Youtube.png' 
 import { Component } from "react";
 import { Image } from "react-bootstrap";
 export default class Footer extends Component {
@@ -11,6 +12,7 @@ export default class Footer extends Component {
     return (
       <div class="container">
         <footer class="footer">
+    
           <div class="row">
             <div class="col col-lg-2">
               <h5>ONLINE SHOPPING</h5>
@@ -59,16 +61,32 @@ export default class Footer extends Component {
               <a href="#">Mytra Insider</a>
             </div>
 
-            <div class="col col-lg-4">
-              <h5>Experinece MeMe App your Phone</h5>
-           
+            <div class="col col-lg-5">
+
+              <h5 >Experinece MeMe App your Phone</h5>
+              <Image src={AppleStore} style={{width:"140px",height:"42px",paddingRight:"10px"}}/>
+              <Image src={GoogleStore} style={{width:"140px",height:"42px",paddingRight:"10px"}}/>
+
               
+              <div class="w-100"></div>
+              <h5>KEEP IN TOUCH</h5>
+<Image src={Youtube} style={{width:"20px",height:"20px", paddingRight:"5px"}}/>
+           
+<Image src={Twitter} style={{width:"20px",height:"20px", paddingRight:"5px"}}/>
+<Image src={Youtube} style={{width:"20px",height:"20px", paddingRight:"5px"}}/>
+           
+           <Image src={Twitter} style={{width:"20px",height:"20px", paddingRight:"5px"}}/>
             </div>
 
             <div class="col col-lg-3">
-              <h5>ONLINE SHOPPING</h5>
+          
+             
+<Image src={Days} style={{width:"48px",height:"40px"}}/>
+<br/>
+<Image src={Original} style={{width:"48px",height:"49px"}}/>
             </div>
           </div>
+        
         </footer>
       </div>
     );
