d7bfd56ee2fecbc47703ee339bb4111f6887cec0
arunSurya-2812 <81164749+arunSurya-2812@users.noreply.github.com>
2021-08-16 16:31:36 +0530

    Navbar Set

--- src/components/Navbard.js
+++ src/components/Navbard.js
@@ -1,6 +1,6 @@
 import "./Components.css";
 import React from "react";
-import { Navbar, Nav, Container,  } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Components.css";
 
 function Navboard() {
@@ -11,34 +11,120 @@ function Navboard() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
-              <Nav.Link href="/" className="nav-item nav-link mx-3">
-                <p>Home</p>
-              </Nav.Link>
+
+            <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000">Home</a>
+                </button>
+              </div>
+
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000/Fiber">Fiber</a>
+                </button>
+                <div class="dropdown-content">
+                  <a href="#">Cotton</a>
+                  <a href="#">Banana Fibre</a>
+                  <a href="#">Linen</a>
+                  <a href="#">Silk</a>
+                  <a href="#">Lotus</a>
+                </div>
+              </div>
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000/Yarn">Yarn</a>
+                </button>
+                <div class="dropdown-content">
+                  <a href="#">Cotton</a>
+                  <a href="#">Poly Cotton</a>
+                  <a href="#">Tencel& Modal</a>
+
+                  <a href="#">Lyocell</a>
+
+                  <a href="#">Recycle</a>
+                </div>
+              </div>
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000/Fabrics">Fabrics</a>
+                </button>
+                <div class="dropdown-content">
+                  <a href="#">Fabrics</a>
+                  <a href="#">SUSTAINABLE FABRICS</a>
+                  <a href="#">ARTISAN MADE</a>
+
+                  <a href="#">FABRICS CRAFTSME</a>
+                </div>
+              </div>
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000/Leathers">Leathers</a>
+                </button>
+                <div class="dropdown-content">
+                  <a href="#">Bags</a>
+                  <a href="#">Accessories</a>
+                  <a href="#">Jacket</a>
+
+                  <a href="#">Footwares</a>
+                </div>
+              </div>
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000/Knits">Knits</a>
+                </button>
+                <div class="dropdown-content">
+                  <a href="#">Knits</a>
+                  <a href="#">Weave</a>
+                  <a href="#">Artisan</a>
+
+                  <a href="#">Eco friendly</a>
+                </div>
+              </div>
+
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000/Brands">Brands</a>
+                </button>
+                <div class="dropdown-content">
+                  <a href="#">Brands</a>
+                  <a href="#">Linen</a>
+                  <a href="#">Artisan</a>
+                  <a href="#">Fibre</a>
+                  <a href="#">Eco friendly</a>
+                </div>
+              </div>
+
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <a href="http://localhost:3000/Ecofriendly">Ecofriendly</a>
+                </button>
+                <div class="dropdown-content">
+                  <a href="#">Fabrics</a>
+                  <a href="#">SUSTAINABLE FABRICS</a>
+                  <a href="#">ARTISAN MADE</a>
+
+                  <a href="#">FABRICS CRAFTSME</a>
+                </div>
+              </div>
+
+              {/*
+              <button class="dropbtn">
+                <a href="http://localhost:3000">Home</a>
+              </button>*      
+
+
+
 
               <Nav.Link
                 href="/Fiber"
                 className="nav-item nav-link mx-3"
                 id="Navline"
               >
-                <p>Fiber</p>
-                <div className="NavDropdown">
-                  
-                    <a  href="#">Cotton</a>
-                    <br />
-                    <a  href="#">Bananaa Fibre</a>
-                    <br />
-                    <a  href="#">Linen</a>
 
-                    <br />
-                    <a   href="#">Silk</a>
-
-                    <br />
-                    <a  href="#">Lotus</a>
-                  
-                </div>
               </Nav.Link>
 
-              <Nav.Link
+             */}
+              {/* <Nav.Link
                 href="/Yarn"
                 className="nav-item nav-link mx-3"
                 id="Navline"
@@ -137,7 +223,7 @@ function Navboard() {
                   </ul>
                 </div>
               </Nav.Link>
-
+             
               <Nav.Link
                 href="/Ecofriendly"
                 className="nav-item nav-link mx-3"
@@ -157,7 +243,7 @@ function Navboard() {
                     <a href="#">FABRICS CRAFTSME</a>
                   </ul>
                 </div>
-              </Nav.Link>
+             </Nav.Link>*/}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
